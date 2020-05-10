20f41b1a4b6f80e6800508117b6a1fe01949f76e
Aayush K.C <aayushkc2158@gmail.com>
2020-05-10 23:58:17 +0545

    Initial Commit

---
+++ Number Objects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Number Objects
+//
+//  Created by Aayush Kc on 5/10/20.
+//  Copyright © 2020 Aayush Kc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
